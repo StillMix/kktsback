9ed8f4bf8479828f516ffb65c006990483ae7cc5
Still Mix <stillmixplaygames@gmail.com>
2025-03-24 09:18:53 +0300

    cors

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ from backup import router as backup_router
 from auth import router as auth_router
 from websocket import router as websocket_router
 from decouple import config  
+from fastapi.middleware.cors import CORSMiddleware
 
+origins = [
+    "http://localhost:5173",  # Ваш Vue.js (Vite) сервер
+    "http://127.0.0.1:5173",
+    "https://stillmix-kktsback-325a.twc1.net", 
+    "https://stillmix-kkts-c97f.twc1.net"
+      # Разрешить ваш основной домен (если нужно)
+]
 
 ALLOWED_IPS = config("ALLOWED_IPS").split(",")  
 VALID_TOKENS = config("VALID_TOKENS").split(",")  
@@ -28,8 +36,21 @@ Base.metadata.create_all(bind=engine)
 # Создаем приложение FastAPI
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Разрешенные источники
+    allow_credentials=True,  # Разрешить передачу куки
+    allow_methods=["*"],  # Разрешенные методы (GET, POST, PUT, DELETE и др.)
+    allow_headers=["*"],  # Разрешенные заголовки
+)
+
+@app.get("/")
+async def home():
+    return {"message": "CORS настроен!"}
+
 app.include_router(student_router)
 app.include_router(teacher_router)
 app.include_router(backup_router)
 app.include_router(auth_router)
 app.include_router(websocket_router)
+
